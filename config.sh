b3579ccfff6a18a6d0cb36057f633faa301230ae
Matthew Brett <matthew.brett@gmail.com>
2016-06-21 13:51:26 -0700

    Refactor to multibuild

---
+++ config.sh
@@ -0,0 +1,16 @@
+# Define custom utilities
+# Test for OSX with [ -n "$IS_OSX" ]
+
+function pre_build {
+    # Any stuff that you need to do before you start building the wheels
+    # Runs in the root directory of this repository.
+    :
+}
+
+function run_tests {
+    # Runs tests on installed distribution from an empty directory
+    python --version
+    python -c 'import statsmodels.api as sm; sm.show_versions();'
+    test_cmd="import sys; import scipy; sys.exit(not scipy.test('full').wasSuccessful())"
+    python -c "$test_cmd"
+}
