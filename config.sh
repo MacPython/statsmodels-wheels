d93938653517a94f0d16244ca0e1b3b0739721b9
Matthew Brett <matthew.brett@gmail.com>
2018-05-15 23:46:31 +0100

    Use basic pytests invocation

--- config.sh
+++ config.sh
@@ -16,6 +16,5 @@ function run_tests {
     # Runs tests on installed distribution from an empty directory
     python --version
     python -c 'import statsmodels.api as sm; sm.show_versions();'
-    test_cmd="import sys; import statsmodels; sys.exit(not statsmodels.test('full', extra_argv=['--exe', '--exclude=test_sarimax', '--exclude=test_structural', '--exclude=test_dynamic_factor', '--exclude=test_varmax']).wasSuccessful())"
-    python -c "$test_cmd"
+    pytest --pyargs statsmodels
 }
